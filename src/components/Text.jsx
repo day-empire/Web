23dcbd21a195df9db1f14d61786272441b5e28bd
Tom Day <tom_day85@hotmail.com>
2025-05-31 03:41:49 +0100

    fix escaping

--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Text({ text }) {
-  return text.split('\n').map((line, index) => (
+  return text.replace("\\n", "\n").split('\n').map((line, index) => (
     <React.Fragment key={index}>
       {line}
       <br />
